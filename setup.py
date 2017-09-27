bd6a4bfae4e08b7d7be9d139beabb50b282391fe
Gonrin <cuongnc.coder@gmail.com>
2017-09-27 21:34:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     install_requires=[
-        'sanic',
+        'sanic', 'passlib'
     ],
     extras_require={
         'dev': [
